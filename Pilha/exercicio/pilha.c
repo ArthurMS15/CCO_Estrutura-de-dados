aad5a899a41c14e26b2d21e77eb2753ee986ab3f
ArthurMS15 <66339390+ArthurMS15@users.noreply.github.com>
2021-06-09 17:28:58 -0300

    Exercicio 2

--- Pilha/exercicio/pilha.c
+++ Pilha/exercicio/pilha.c
@@ -1,130 +1,83 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct sElemento{
     struct sElemento *next;
     struct sElemento *prev;
-    int dado;
+    char dado;
+    int size;
 } Elemento;
 
-typedef struct sPilha{
-    struct sElemento *head;
-    struct sElemento *tail;
-    int size;
-} Pilha;
-
-
-Pilha *alocaPilha();
-Elemento *alocaElemento(int);
-void imprimePilha(Pilha*);
-int empty(Pilha*);
-void stacktop(Pilha*);
-void push(Pilha*, int);
-int pop(Pilha*);
+int empty(Elemento* t);
 
 int main(){
-    Pilha *p;
-    p=alocaPilha();
-    push(p, 1);
-    push(p, 2);
-    push(p, 3);
-    imprimePilha(p);
-    stacktop(p);
-    pop(p);
-    imprimePilha(p);
-    stacktop(p);
+    char str[255];  
+    Elemento cabeca; 
+    Elemento *topo; 
+    topo = &cabeca; 
+    topo->next = NULL; 
+    printf("Informe a string: "); 
+    gets(str); 
+    verify(str, topo);
+    return 0; 
 }
 
-Pilha *alocaPilha(){
-    Pilha *p;
-    p=(Pilha*)malloc(sizeof(Pilha));
-    p->head=NULL;
-    p->tail=NULL;
-    p->size=0;
-    return p;
+void push(Elemento* t, char dado){
+    Elemento *e; 
+    e = malloc( sizeof (Elemento)); 
+    e->dado = dado; 
+    e->next = t->next; 
+    t->next = e;
+    t->size++;
 }
 
-Elemento *alocaElemento(int d){
-    Elemento *e;
-    e=(Elemento*)malloc(d*sizeof(Elemento));
-    e->next=NULL;
-    e->prev=NULL;
-    e->dado=d;
-    return e;
+char pop(Elemento* t){
+    char c; 
+    Elemento *pt; 
+    pt = t->next; 
+    c = pt->dado; 
+    t->next = pt->next; 
+    free(pt); 
+    return c; 
 }
 
-void imprimePilha(Pilha* p){
-    Elemento *ea=p->tail; //elemento auxiliar
-    printf("NULL\n");
-    while(ea!=NULL){
-        printf("%d\n", ea->dado);
-        ea=ea->prev;
+int verify(char* str, Elemento* t) { 
+    char aux;
+    for (int i=0; i<strlen(str); i++){ 
+        if (str == '( ' || str == '[ ' || str == '{' ){ 
+            push(t, str);
+            t->size++; 
+        }
     }
-    printf("NULL\n");
-}
+    for (int i=0; i<strlen(str); i++){
+        if (str == ') ' || str == '] ' || str == '}' ){
+            if(empty(t)==1){
+                return -1;
+            } else {
+                aux = pop(t);
+                t->size--; 
+                if(aux != '( ' && aux != '[ ' && aux != '{' ){
+                    return -2;
+                }
+            }
+        }
+    }
+    if(empty==0){
+        return -3;
+    } else {
+        printf("A string eh valida");
+        return 1;
+    }
+} 
 
-int empty(Pilha* p){
-    if(p->size==0){
-        printf("Sim a pilha esta vazia\n");
+int empty(Elemento* t){
+    if(t->size==0){
+        printf("Sim a lista esta vazia\n");
         return 1;
     } else {
-        printf("Nao a pilha nao esta vazia\n");
+        printf("Nao a lista nao esta vazia\n");
         return 0;
     }
 }
-
-void stacktop(Pilha* p){
-    printf("Elemento do topo: %d\n", p->tail->dado);
-}
-
-void push(Pilha* p, int dado){
-    Elemento *ea=alocaElemento(dado);
-    Elemento *pivo=p->tail;
-    ea->dado=dado;
-    if(p->size==0){
-        p->head=ea;
-        p->tail=ea;
-    } else {
-        ea->next=pivo->next;
-        ea->prev=pivo;
-        if(pivo->next==NULL){
-            p->tail=ea;
-        } else {
-            pivo->next->prev=ea;
-        }
-        pivo->next=ea;
-    }
-    p->size++;
-}
-
-int pop(Pilha* p){
-    int dado;
-    Elemento *e=p->tail;
-    if((p!=NULL) && (p->size>0)){
-        if(e==p->head){
-            p->head=e->next;
-            if(p->head==NULL){
-                p->tail=NULL;
-            } else {
-                e->next->prev=NULL;
-            }
-        } else {
-            e->prev->next=e->next;
-            if(e->next==NULL){
-                p->tail=e->prev;
-            } else {
-                e->next->prev=e->prev;
-            }
-        }
-        dado=e->dado;
-        free(e);
-        p->size--;
-        return dado;
-    }
-    printf("Erro: elemento NULL ou lista vazia\n");
-    return -1;
-}
-
-void freePilha(Pilha* p){
-    
-}
+ 
