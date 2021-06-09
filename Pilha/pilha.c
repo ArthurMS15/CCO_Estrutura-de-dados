821f867517d86d7e1efb9e719ad32c582bd68cd4
ArthurMS15 <66339390+ArthurMS15@users.noreply.github.com>
2021-06-09 13:53:43 -0300

    Update pilha.c

--- Pilha/pilha.c
+++ Pilha/pilha.c
@@ -1,8 +1,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct sElemento{
+    struct sElemento *next;
+    struct sElemento *prev;
+    int dado;
+} Elemento;
+
+typedef struct sPilha{
+    struct sElemento *head;
+    struct sElemento *tail;
+    int size;
+} Pilha;
+
+
+Pilha *alocaPilha();
+Elemento *alocaElemento(int);
+void imprimePilha(Pilha*);
+int empty(Pilha*);
+void stacktop(Pilha*);
+
 int main(){
 
+}
+
+Pilha *alocaPilha(){
+    Pilha *p;
+    p=(Pilha*)malloc(sizeof(Pilha));
+    p->head=NULL;
+    p->tail=NULL;
+    p->size=0;
+    return p;
+}
+
+Elemento *alocaElemento(int d){
+    Elemento *e;
+    e=(Elemento*)malloc(d*sizeof(Elemento));
+    e->next=NULL;
+    e->prev=NULL;
+    e->dado=d;
+    return e;
+}
+
+void imprimePilha(Pilha* p){
+    Elemento *ea=p->tail; //elemento auxiliar
+    printf("NULL\n");
+    while(ea!=NULL){
+        printf("%d\n", ea->dado);
+        ea=ea->prev;
+    }
+    printf("NULL\n");
+}
+
+int empty(Pilha* p){
+    if(p->size==0){
+        printf("Sim a pilha esta vazia\n");
+        return 1;
+    } else {
+        printf("Nao a pilha nao esta vazia\n");
+        return 0;
+    }
+}
+
+void stacktop(Pilha* p){
+    printf("%d", p->tail->dado);
 }
 
 //lastin firstout - vertical
@@ -13,6 +74,6 @@ pilha - s
 i - item
 pop(s) x=s.items[stop--]; em vetor
 overflow e underflow
-empty(s) - ver se a pilha ta vazia
-stacktop(s) - retorna uma consulta do elemtno q esta no topo da pilha mas sem remover
+
+stacktop(s) - retorna uma consulta do elemento q esta no topo da pilha mas sem remover
 */
