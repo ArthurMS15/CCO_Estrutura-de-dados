2c91cb4800170fb609040eea54230d5ce57715e0
ArthurMS15 <66339390+ArthurMS15@users.noreply.github.com>
2021-06-08 21:45:04 -0300

    anotação pilhas

---
+++ Pilha/pilha.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+}
+
+//lastin firstout - vertical
+//push() - empilhar - inserir
+//pop() - desempilhar - remover
+/*push(s,i) s.items[++s.top]=i em vetor
+pilha - s
+i - item
+pop(s) x=s.items[stop--]; em vetor
+overflow e underflow
+empty(s) - ver se a pilha ta vazia
+stacktop(s) - retorna uma consulta do elemtno q esta no topo da pilha mas sem remover
+*/
