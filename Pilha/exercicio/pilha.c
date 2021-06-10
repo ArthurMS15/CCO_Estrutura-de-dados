e2c8b322028a5260b62fb6289eff9de02aaa6150
ArthurMS15 <66339390+ArthurMS15@users.noreply.github.com>
2021-06-09 22:07:24 -0300

    Update

--- Pilha/exercicio/pilha.c
+++ Pilha/exercicio/pilha.c
@@ -9,7 +9,12 @@ typedef struct sElemento{
     int size;
 } Elemento;
 
-int empty(Elemento* t);
+void push(Elemento*, char);
+char pop(Elemento*);
+int verify(char*, Elemento*);
+int empty(Elemento*);
+void imprimePilha(Elemento*);
+
 
 int main(){
     char str[255];  
@@ -23,9 +28,18 @@ int main(){
     return 0; 
 }
 
+Elemento *alocaElemento(char d){
+    Elemento *e;
+    e=(Elemento*)malloc(d*sizeof(Elemento));
+    e->next=NULL;
+    e->prev=NULL;
+    e->dado=d;
+    e->size=0;
+    return e;
+}
+
 void push(Elemento* t, char dado){
-    Elemento *e; 
-    e = malloc( sizeof (Elemento)); 
+    Elemento *e=alocaElemento(dado);
     e->dado = dado; 
     e->next = t->next; 
     t->next = e;
@@ -37,18 +51,19 @@ char pop(Elemento* t){
     pt = t->next; 
     c = pt->dado; 
     t->next = pt->next; 
-    free(pt); 
+    free(pt);
     return c; 
 }
 
-int verify(char* str, Elemento* t) { 
+int verify(char* str, Elemento* t){ 
     char aux;
     for (int i=0; i<strlen(str); i++){ 
-        if (str == '(' || str == '[' || str == '{' ){ 
-            push(t, str);
-            t->size++; 
+        if (str[i] == '(' || str[i]  == '[' || str[i]  == '{' ){ 
+            push(t, str[i]);
+            t->size++;
+            printf("%c\n", t->dado);
         }
-        if (str == ')' || str == ']' || str == '}' ){
+        if (str == ')' || str[i] == ']' || str[i] == '}' ){
             if(empty(t)==1){
                 return -1;
             } else {
@@ -77,6 +92,3 @@ int empty(Elemento* t){
     }
 }
  
- void imprimePilha(Elemento* e){
-     
- }
