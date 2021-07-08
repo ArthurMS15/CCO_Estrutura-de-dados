c07a5cbec96b1d04f456b06046fd3106139ec6f8
ArthurMS15 <66339390+ArthurMS15@users.noreply.github.com>
2021-07-07 23:17:21 -0300

    Update

--- projeto/hashlinkedlist.c
+++ projeto/hashlinkedlist.c
@@ -5,18 +5,18 @@
 
 typedef struct node {
     char* name;
-    node* next;
-    node* prev;
+    struct node* next;
+    struct node* prev;
 } node;
 
-int hashFunction(char*);
+node* hashtable[M];
+
+unsigned int hash(char*);
 unsigned int create(char*);
 void destroy(node*);
-void removeSpec(char*);
 int find(char*);
 
 int main(){
-    node* hashtable[M];
     for (int i = 0; i < M; i++){
         hashtable[i] = NULL;
     }
@@ -36,6 +36,7 @@ int main(){
         //printf("%d", aux);
     }
 
+    fprintf(stdout, "%s\n", hashtable[23]->name);
 
     fclose(file);
 
@@ -49,45 +50,25 @@ int main(){
 }
 
 unsigned int create(char* name){
-    int n = hashFunction(name);
-    node* hashtable[M];
-    node* entry = hashtable[M];
-    node* top[M];
-    for (int i = 0; i < M; i++) {
-        hashtable[i] = NULL;
-        top[i] = NULL;
-    }
-
-    if(entry == NULL){
-        entry = malloc(sizeof(node));
-        if(entry == NULL){
-            exit(1);
-        } else {
-            entry->name = name;
-            entry->next = NULL;
-            entry->prev = NULL;
-            hashtable[n] = entry;
-            top[n] = entry;
-        }
+    int n = hash(name);
+    node* nnode = malloc(sizeof(node));
+    if(nnode == NULL){
+        exit(1);
     } else {
-        while (entry != NULL){
-            entry = entry->next;
-        }
-        entry = malloc(sizeof(node));
-        if(entry == NULL){
-            exit(1);
-        } else {
-            entry->name = name;
-            entry->next = NULL;
-            entry->prev = top[n];
-            top[n]->next = entry;
-            top[n] = entry;
-        }
+        nnode->name = name;
+        nnode->next = hashtable[n];
+        hashtable[n] = nnode;
+        //printf("%s", hashtable[23]->name);
     }
     return n;
 }
 
-int hashFunction(char* str){
+unsigned int hash(char* str){
+    //int sum=0;
+    //for(int i=0; str[i] != '\0'; i++){
+    //    sum += str[i];
+    //}
+    //return sum % M;
     int acumulador=0;
     for(int i=0; i < strlen(str); i++){
         int aux = str[i];
@@ -106,49 +87,14 @@ void destroy(node* node){
     return;
 }
 
-void removeSpec(char* name){
-    node* hashtable[M];
-    node* top[M];
-    int n = hashFunction(name);
-    node* entry = hashtable[n];
-    if (entry->name != name || entry == NULL) {
-        printf("Não foi encontrado o nome: %s na chave: %d OU a chave está vazia\n", name, n);
-        return;
-    }
-    // if some values are present at that key &
-    // traversing the list and removing all values
-    while (entry != NULL) {
-        if (entry->next == NULL) {
-            if (entry->prev == NULL) {
-                hashtable[n] = NULL;
-                top[n] = NULL;
-                destroy(entry);
-                break;
-            }
-            else {
-                top[n] = entry->prev;
-                top[n]->next = NULL;
-                destroy(entry);
-                entry = top[n];
-            }
-        }
-        entry = entry->next;
-    }
-    printf("Elemento excluído com sucesso da chave: %d\n", n);
-}
-
 int find(char* name){
-    //aplying hash to find
-    //index for given name
-    int n = hashFunction(name);
-    node* hashtable[M];
+    int n = hash(name);
     if(hashtable[n] == NULL){
         for(node* aux=hashtable[n]; aux != NULL; aux=aux->next){
             if(aux->name == name){
-                printf("Elemento encontrado com sucesso na chave: %d\n", n);
                 return 1;
             }
         }
-    }        
+    }
     return 0;
 }
