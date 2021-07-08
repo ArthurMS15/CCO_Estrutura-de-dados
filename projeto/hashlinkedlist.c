0e305f7b33a07f2126ba1d37a034080b66b0c4ff
ArthurMS15 <66339390+ArthurMS15@users.noreply.github.com>
2021-07-08 17:37:34 -0300

    Update

--- projeto/hashlinkedlist.c
+++ projeto/hashlinkedlist.c
@@ -1,33 +1,34 @@
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define M 26
+#define M 3
 
-typedef struct node {
-    char* key;
-    char* value;
-    struct node* next; // colisão
-    struct node* prev; 
-} node;
+typedef struct entry_t {
+    char *key;
+    char *value;
+    struct entry_t* next; // colisão
+} entry_t;
 // 1 entry in the hash table, a key of value at a pointer to another entry
 
 typedef struct {
-    node **entries;
-} ht;
+    entry_t **entries;
+} ht_t;
 //hash tabel itself, array of pointers to an entry
 
-ht *ht_create(void);
-unsigned int hash(char* str);
-node *ht_pair(const char *key, const char *value);
-node *ht_get(ht *hashtable, const char *key);
-void ht_dump(ht *hashtable);
-void createNode(ht *hashtable, const char *key, const char *value);
-void destroy(node* node);
-void freeHashTable(ht *hashtable);
+ht_t *ht_create(void);
+unsigned int hash(const char *);
+void ht_set(ht_t *, const char *, const char *);
+entry_t *ht_pair(const char *, const char *);
+char *ht_get(ht_t *, const char *);
+void ht_dump(ht_t *);
+void createNode(ht_t *, const char *, const char *);
+void destroy(entry_t*);
+void freeHashTable(ht_t *);
 
 int main(){
-    ht *ht = ht_create();
+    ht_t *ht = ht_create();
     //setando ponteiros
     FILE *file = fopen("teste.txt", "r");
 
@@ -43,110 +44,137 @@ int main(){
         createNode(ht, "1", nome);
     }*/
 
+    createNode(ht, "1", "em");
+    createNode(ht, "2", "russian");
+    createNode(ht, "3", "pizza");
+    createNode(ht, "4", "doge");
+    createNode(ht, "5", "pyro");
+    createNode(ht, "6", "joost");
+    createNode(ht, "7", "kalix");
+
     ht_dump(ht);
 
     fclose(file);
-    freeHashTable(ht);
+    //freeHashTable(ht);
 
     return 0;
 }
 
-ht *ht_create(void){
-    ht *hashtable = malloc(sizeof(ht)*1); //ponteiro para a hashtable
+ht_t *ht_create(void){
+    ht_t *hashtable = malloc(sizeof(ht_t) * 1); //ponteiro para a hashtable
 
-    hashtable->entries = malloc(sizeof(node*)*M); //x numbers of pointers to an entry/node (26 ponteiros, baseado no M)
+    hashtable->entries = malloc(sizeof(entry_t*) * M); 
+    //x numbers of pointers to an entry/node (26 ponteiros, baseado no M)
 
-    for(int i=0;i<M;++i){
+    int i = 0;
+    for(; i < M; ++i){
         hashtable->entries[i] = NULL;
     } //inicializando os ponteiros
+
     return hashtable;
 }
 
-unsigned int hash(char* str){
-    int acumulador=0;
-    for(int i=0; i < strlen(str); i++){
-        acumulador = (31 * acumulador + str[i]);
+unsigned int hash(const char* key){
+    unsigned long int value =0 ;
+    unsigned int i=0;
+    unsigned int key_len = strlen(key);
+
+    for(; i < key_len; ++i){
+        value = 31 * value + key[i];
     }
-    acumulador = acumulador % M;
+
+    value = value % M;
     //valor é maior que 0 e menor que M;
-    return acumulador;
+    return value;
 }
 
-node *ht_pair(const char *key, const char *value){
-    node *node = malloc(sizeof(node) * 1);
-    node->key = malloc(sizeof(key) + 1);
-    node->value = malloc(sizeof(value) + 1);
+void ht_set(ht_t *hashtable, const char *key, const char *value){
+    unsigned int bucket = hash(key);
 
-    strcpy(node->key, key);
-    strcpy(node->value, value);
+    entry_t *entry = hashtable->entries[bucket];
 
-    node->next=NULL;
+    if(entry == NULL){
+        hashtable->entries[bucket] = ht_pair(key, value);
+        return;
+    }
 
-    return node;
+    entry_t *prev;
+
+    while (entry != NULL){
+
+        if(strcmp(entry->key, key) == 0){
+            free(entry->value);
+            entry->value = malloc(strlen(value) + 1);
+            strcpy(entry->value, value);
+            return;
+        }
+
+        prev = entry;
+        entry = prev->next;
+    }
+
+    prev->next = ht_pair(key, value);
 }
 
-node *ht_get(ht *hashtable, const char *key){
+
+entry_t *ht_pair(const char *key, const char *value){
+    entry_t *entry = malloc(sizeof(entry) * 1);
+    entry->key = malloc(strlen(key) + 1);
+    entry->value = malloc(strlen(value) + 1);
+
+    strcpy(entry->key, key);
+    strcpy(entry->value, value);
+
+    entry->next=NULL;
+
+    return entry;
+}
+
+char *ht_get(ht_t *hashtable, const char *key){
     unsigned int slot = hash(key);
-    node *node = hashtable->entries[slot];
 
-    if(node==NULL){
+    entry_t *entry = hashtable->entries[slot];
+
+    if(entry == NULL){
         return NULL;
     }
 
-    while(node != NULL){
-        if(strcmp(node->key, key) == 0){
-            return node->value;
+    while(entry != NULL){
+        if(strcmp(entry->key, key) == 0){
+            return entry->value;
         } 
-        node = node->next;
+        entry = entry->next;
     }
 
     return NULL;
 }
 
-void ht_dump(ht *hashtable){
+void ht_dump(ht_t *hashtable){
     for(int i=0;i<M;++i){
-        node *node = hashtable->entries[i];
+        entry_t *entry = hashtable->entries[i];
 
-        if(node==NULL){
+        if(entry == NULL){
             continue;
         }
         printf("slot[%4d]: ", i);
-        for(;;){
-            printf("%s=%s ", node->key, node->value);
 
-            if(node->next==NULL){
+        for(;;){
+            printf("%s=%s ", entry->key, entry->value);
+
+            if(entry->next==NULL){
                 break;
             }
-            node = node->next;
+
+            entry = entry->next;
+
         }
+
         printf("\n");
+
     }
 }
 
-void createNode(ht *hashtable, const char *key, const char *value){
-    unsigned int slot = hash(key);
-    node *node = hashtable->entries[slot];
-    if(node == NULL){
-        hashtable->entries[slot] = ht_pair(key, value);
-        return;
-    }
-
-    while (node != NULL){
-        if(strcmp(node->key, key) == 0){
-            free(node->value);
-            node->value = malloc(strlen(value) + 1);
-            strcpy(node->value, value);
-            return;
-        }
-
-        node->prev = node;
-        node = node->prev->next;
-    }
-
-    node->prev->next = ht_pair(key, value);
-}
-
-void destroy(node* node){
+void destroy(entry_t* node){
     if(node->next==NULL){
         free(node);
     } else {
@@ -156,9 +184,9 @@ void destroy(node* node){
     return;
 }
 
-void freeHashTable(ht *hashtable){
+void freeHashTable(ht_t *hashtable){
     for(int i=0;i<M;++i){
-        node *node = hashtable->entries[i];
+        entry_t *node = hashtable->entries[i];
 
         if(node==NULL){
             continue;
